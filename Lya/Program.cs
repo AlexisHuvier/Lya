1fd66537d6f9b7a27957bc8cb2daae1311be465f
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-07-10 23:59:57 +0200

    First commit

---
+++ Lya/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
