e00c43de50ab4eb2577e6d9fda8c221bc5669397
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-12-10 17:05:04 +0100

    Move Env and create Scope

---
+++ Lya/Utils/Env.cs
@@ -0,0 +1,42 @@
+﻿using Lya.Objects;
+using Lya.Objects.Function;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lya.Utils;
+
+public class Env
+{
+    
+    public static readonly List<string> KeywordTypes = new() { "int", "float", "bool", "string" };
+    public static readonly List<string> Keywords = new() { "if" };
+
+    public readonly Stack<Scope> Scopes;
+
+    public Env()
+    {
+        Scopes = new Stack<Scope>();
+        Scopes.Push(new Scope(new List<IFunction>
+        {
+            new PrintFunction(), new PrintEnvFunction()
+        }, new List<Variable>
+        {
+            new("true", VariableType.Bool, true), new("false", VariableType.Bool, false)
+        }));
+    }
+
+    public Scope GetCurrentScope() => Scopes.ElementAt(Scopes.Count - 1);
+    
+    public void AddGlobalFunctions(IEnumerable<IFunction> functions) => Scopes.ElementAt(0).AddFunctions(functions);
+    public void AddGlobalFunction(IFunction function) => Scopes.ElementAt(0).AddFunction(function);
+    public void AddGlobalVariables(IEnumerable<Variable> vars) => Scopes.ElementAt(0).AddVariables(vars);
+    public void AddGlobalVariable(Variable var) => Scopes.ElementAt(0).AddVariable(var);
+
+    public IFunction GetFunction(string name) => Scopes.Where(scope => scope.IsFunctionDefine(name)).Select(scope => scope.GetFunction(name)).FirstOrDefault();
+    public Variable GetVariable(string name) => Scopes.Where(scope => scope.IsVariableDefine(name)).Select(scope => scope.GetVariable(name)).FirstOrDefault();
+    
+    public bool IsFunctionDefine(string name) => Scopes.Any(scope => scope.IsFunctionDefine(name));
+    public bool IsVariableDefine(string name) => Scopes.Any(scope => scope.IsVariableDefine(name));
+    public bool IsDefine(string name) => IsFunctionDefine(name) || IsVariableDefine(name);
+}
---
+++ Lya/Utils/Scope.cs
@@ -0,0 +1,37 @@
+﻿using Lya.Objects;
+using Lya.Objects.Function;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lya.Utils;
+
+public class Scope
+{
+    public readonly List<IFunction> Functions;
+    public readonly List<Variable> Variables;
+
+    public Scope()
+    {
+        Functions = new List<IFunction>();
+        Variables = new List<Variable>();
+    }
+
+    public Scope(List<IFunction> functions, List<Variable> variables)
+    {
+        Functions = functions;
+        Variables = variables;
+    }
+
+    public void AddFunctions(IEnumerable<IFunction> functions) => Functions.AddRange(functions);
+    public void AddFunction(IFunction function) => Functions.Add(function);
+    public bool IsFunctionDefine(string name) => Functions.Any(x => x.Name == name);
+    public IFunction GetFunction(string name) => Functions.Find(x => x.Name == name);
+    
+    public void AddVariables(IEnumerable<Variable> variables) => Variables.AddRange(variables);
+    public void AddVariable(Variable variable) => Variables.Add(variable);
+    public bool IsVariableDefine(string name) => Variables.Any(x => x.Name == name);
+    public Variable GetVariable(string name) => Variables.Find(x => x.Name == name);
+
+    public bool IsDefine(string name) => IsFunctionDefine(name) || IsVariableDefine(name);
+}
