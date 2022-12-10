dc4b274271c755e9133caddb2da39ba651a4c57f
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-12-10 17:07:53 +0100

    Create LyaExamples

---
+++ LyaExamples/Program.cs
@@ -0,0 +1,11 @@
+﻿using Lya;
+
+namespace LyaExamples; // Note: actual namespace depends on the project name.
+
+internal class Program
+{
+    static void Main()
+    {
+        Interpreter.Run("Examples/basic.lya");
+    }
+}
