77623e758c691a571999c0eef5800207ebb4d488
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-12-10 13:01:40 +0100

    Work on Token

--- Lya/Utils/Token.cs
+++ Lya/Utils/Token.cs
@@ -12,6 +12,22 @@ public class Token
 
     public override string ToString()
     {
-        return $"{nameof(Value)}: {Value}, {nameof(Type)}: {Type}, {nameof(Line)}: {Line}, {nameof(Column)}: {Column}";
+        return $"{nameof(Value)}: {Value}, {nameof(Type)}: {Type}, {nameof(Line)}: {Line}, {nameof(Column)}: {Column}, {nameof(File)}: {File}";
     }
+
+    public static bool operator ==(Token t1, Token t2) => t1?.Value == t2?.Value && t1?.Type == t2?.Type &&
+                                                          t1?.File == t2?.File && t1?.Line == t2?.Line &&
+                                                          t1?.Column == t2?.Column;
+    public static bool operator !=(Token t1, Token t2) => !(t1 == t2);
+    
+    protected bool Equals(Token other) => Value == other.Value && Type == other.Type && File == other.File && Line == other.Line && Column == other.Column;
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == GetType() && Equals((Token)obj);
+    }
+
+    public override int GetHashCode() => HashCode.Combine(Value, (int)Type, File, Line, Column);
 }
