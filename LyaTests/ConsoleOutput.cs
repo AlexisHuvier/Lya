d1a515bc0f57fe7454b4bbcea622c340ac696e13
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-12-12 09:39:18 +0100

    Create ConsoleOutput.cs

---
+++ LyaTests/ConsoleOutput.cs
@@ -0,0 +1,24 @@
+﻿namespace LyaTests;
+
+public class ConsoleOutput : IDisposable
+{
+    StringWriter _stringWriter;
+    TextWriter _originalOut;
+
+    public ConsoleOutput()
+    {
+        _stringWriter = new StringWriter();
+        _originalOut = Console.Out;
+        Console.SetOut(_stringWriter);
+    }
+
+    public string GetOut() => _stringWriter.ToString();
+    public string[] GetOutLines() => _stringWriter.ToString().Replace("\r", "").Split("\n");
+    
+    public void Dispose()
+    {
+        Console.SetOut(_originalOut);
+        _stringWriter.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
