9f23eaa051df1106746960df2f3dbcb0d5fed8c4
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-12-12 09:39:39 +0100

    Create CoreFunctionsTests

--- LyaTests/CoreFunctionsTests.cs
+++ LyaTests/CoreFunctionsTests.cs
@@ -1,6 +1,33 @@
-﻿namespace LyaTests;
+﻿using Lya;
+
+namespace LyaTests;
 
 public class CoreFunctionsTests
 {
-    
+    [Test]
+    public void PrintTests()
+    {
+        var outConsole = new ConsoleOutput();
+        Interpreter.Run("print(\"testing\");");
+        Assert.That(outConsole.GetOutLines()[0], Is.EqualTo("testing"));
+        Interpreter.Run("print(\"testing with\", \"many arguments\");");
+        Assert.That(outConsole.GetOutLines()[1], Is.EqualTo("testing with many arguments"));
+        Interpreter.Run("int age = 18;print(\"Vous avez\", age, \"ans.\");");
+        Assert.That(outConsole.GetOutLines()[2], Is.EqualTo("Vous avez 18 ans."));
+    }
+
+    [Test]
+    public void PrintEnvTests()
+    {
+        var outConsole = new ConsoleOutput();
+        Interpreter.Run("int i = 5; printenv();");
+        Assert.Multiple(() =>
+        {
+            Assert.That(outConsole.GetOutLines()[0], Is.EqualTo("Env :"));
+            Assert.That(outConsole.GetOutLines()[1], Is.EqualTo("== Scope =="));
+            Assert.That(outConsole.GetOutLines()[2], Is.EqualTo("Functions : print, printenv"));
+            Assert.That(outConsole.GetOutLines()[3], Is.EqualTo("Variables : true (True), false (False), i (5)"));
+            Assert.That(outConsole.GetOutLines()[4], Is.EqualTo("==========="));
+        });
+    }
 }
