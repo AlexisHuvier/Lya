d85ca027b7a6fb15ddde0cc04b858fd56000f1c2
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-12-12 09:39:27 +0100

    Fix LexerTests

--- LyaTests/LexerTests.cs
+++ LyaTests/LexerTests.cs
@@ -10,8 +10,8 @@ public class LexerTests
     {
         Assert.Multiple(() =>
         {
-            Assert.That(Lexer.Tokenize("int i = 0;", "_")[1].Column, Is.EqualTo(5));
-            Assert.That(Lexer.Tokenize("int i = 0;\nprint(i);", "_")[5].Line, Is.EqualTo(2));
+            Assert.That(Lexer.Tokenize("int i = 0;")[1].Column, Is.EqualTo(5));
+            Assert.That(Lexer.Tokenize("int i = 0;\nprint(i);")[5].Line, Is.EqualTo(2));
         });
     }
     
@@ -20,17 +20,17 @@ public class LexerTests
     {
         Assert.Multiple(() =>
         {
-            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.KeywordType, Value = "int" }, Is.EqualTo(Lexer.Tokenize("int", "_")[0]));
-            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Brace, Value = "{" }, Is.EqualTo(Lexer.Tokenize("{", "_")[0]));
-            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Comma, Value = "," }, Is.EqualTo(Lexer.Tokenize(",", "_")[0]));
-            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Hook, Value = "[" }, Is.EqualTo(Lexer.Tokenize("[", "_")[0]));
-            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Paren, Value = "(" }, Is.EqualTo(Lexer.Tokenize("(", "_")[0]));
-            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Identifier, Value = "identifier" }, Is.EqualTo(Lexer.Tokenize("identifier", "_")[0]));
-            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Keyword, Value = "if" }, Is.EqualTo(Lexer.Tokenize("if", "_")[0]));
-            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Number, Value = "4" }, Is.EqualTo(Lexer.Tokenize("4", "_")[0]));
-            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Operator, Value = "=" }, Is.EqualTo(Lexer.Tokenize("=", "_")[0]));
-            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.SemiColon, Value = ";" }, Is.EqualTo(Lexer.Tokenize(";", "_")[0]));
-            Assert.That(Lexer.Tokenize(" ", "_"), Is.Empty);
+            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.KeywordType, Value = "int" }, Is.EqualTo(Lexer.Tokenize("int")[0]));
+            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Brace, Value = "{" }, Is.EqualTo(Lexer.Tokenize("{")[0]));
+            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Comma, Value = "," }, Is.EqualTo(Lexer.Tokenize(",")[0]));
+            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Hook, Value = "[" }, Is.EqualTo(Lexer.Tokenize("[")[0]));
+            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Paren, Value = "(" }, Is.EqualTo(Lexer.Tokenize("(")[0]));
+            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Identifier, Value = "identifier" }, Is.EqualTo(Lexer.Tokenize("identifier")[0]));
+            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Keyword, Value = "if" }, Is.EqualTo(Lexer.Tokenize("if")[0]));
+            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Number, Value = "4" }, Is.EqualTo(Lexer.Tokenize("4")[0]));
+            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.Operator, Value = "=" }, Is.EqualTo(Lexer.Tokenize("=")[0]));
+            Assert.That(new Token { Column = 1, Line = 1, File = "_", Type = TokenType.SemiColon, Value = ";" }, Is.EqualTo(Lexer.Tokenize(";")[0]));
+            Assert.That(Lexer.Tokenize(" "), Is.Empty);
         });
     }
 }
