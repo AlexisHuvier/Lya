ffaa340293e99e814955348cabe06ec156ad4226
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-12-12 21:15:30 +0100

    Fix Program

--- Lya/Program.cs
+++ Lya/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using Lya.Utils;
+using System;
+using System.IO;
 
 namespace Lya;
 
@@ -7,6 +9,24 @@ class Program
     static void Main(string[] args)
     {
         if (args.Length == 1 && File.Exists(args[0]))
-            Interpreter.Run(args[0]);
+            Interpreter.RunFile(args[0]);
+        else
+        {
+            var env = new Env();
+            while (true)
+            {
+                Console.Write($"Lya {Interpreter.Version} >>> ");
+                var line = Console.ReadLine();
+                if (line == "exit")
+                    break;
+                try
+                {
+                    Interpreter.Run(line, env);
+                }
+                catch (Exception) { 
+                    // ignored
+                }
+            }
+        }
     }
 }
